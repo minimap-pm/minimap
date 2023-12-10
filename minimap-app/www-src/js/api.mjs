b442bd1930fa96cbfb9b5e0a363fa139667a4b51
Josh Junon <git@josh.junon.me>
2023-12-10 18:54:13 +0100

    add javascript API for interacting with tauri commands

---
+++ minimap-app/www-src/js/api.mjs
@@ -0,0 +1,196 @@
+import { invoke } from '@tauri-apps/api/tauri';
+
+class Ticket {
+	constructor(workspaceId, ticketId, prefix) {
+		this._workspaceId = workspaceId;
+		this._ticketId = ticketId;
+		this._prefix = prefix;
+	}
+
+	get slug() {
+		return this._ticketId;
+	}
+
+	/*async*/ _send(command, data = {}) {
+		return invoke(`${this._prefix}_${command}`, {
+			workspace: this._workspaceId,
+			ticket: this._ticketId,
+			...data
+		});
+	}
+
+	/*async*/ getTitle() {
+		return this._send('ticket_title');
+	}
+
+	/*async*/ setTitle(title) {
+		return this._send('ticket_set_title', { title });
+	}
+
+	/*async*/ addComment(comment) {
+		return this._send('ticket_add_comment', { comment });
+	}
+
+	/*async*/ getComments() {
+		return this._send('ticket_comments');
+	}
+
+	/*async*/ getStatus() {
+		return this._send('ticket_status');
+	}
+
+	/*async*/ setStatus(status) {
+		return this._send('ticket_set_status', { status });
+	}
+
+	/*async*/ upsertAttachment(name, data) {
+		if (data instanceof ArrayBuffer) {
+			data = new Uint8Array(data);
+		}
+
+		if (data instanceof Uint8Array) {
+			return this._send('ticket_upsert_attachment', { name, data });
+		}
+
+		if (data instanceof String || typeof data === 'string') {
+			return this._send('ticket_upsert_attachment_filepath', {
+				name,
+				filepath: data
+			});
+		}
+	}
+
+	/*async*/ removeAttachment(name) {
+		return this._send('ticket_remove_attachment', { name });
+	}
+
+	/*async*/ getAttachment(name) {
+		return this._send('ticket_attachment', { name });
+	}
+
+	/*async*/ getAttachmentBase64(name) {
+		return this._send('ticket_attachment_base64', { name });
+	}
+
+	/*async*/ getDependencies() {
+		return this._send('ticket_dependencies');
+	}
+
+	/*async*/ addDependency(origin, endpoint) {
+		return this._send('ticket_add_dependency', { origin, endpoint });
+	}
+
+	/*async*/ removeDependency(origin, endpoint) {
+		return this._send('ticket_remove_dependency', { origin, endpoint });
+	}
+}
+
+class Project {
+	constructor(workspaceId, projectId, prefix) {
+		this._workspaceId = workspaceId;
+		this._projectId = projectId;
+		this._prefix = prefix;
+	}
+
+	/*async*/ _send(command, data = {}) {
+		return invoke(`${this._prefix}_${command}`, {
+			workspace: this._workspaceId,
+			project: this._projectId,
+			...data
+		});
+	}
+
+	/*async*/ getName() {
+		return this._send('project_name');
+	}
+
+	/*async*/ setName(name) {
+		return this._send('project_set_name', { name });
+	}
+
+	/*async*/ getDescription() {
+		return this._send('project_description');
+	}
+
+	/*async*/ setDescription(description) {
+		return this._send('project_set_description', { description });
+	}
+
+	async createTicket() {
+		const slug = await this._send('project_create_ticket');
+		return new Ticket(this._workspaceId, slug, this._prefix);
+	}
+}
+
+class Workspace {
+	constructor(id, prefix) {
+		this._id = id;
+		this._prefix = prefix;
+	}
+
+	/*async*/ _send(command, data = {}) {
+		return invoke(`${this._prefix}_${command}`, {
+			workspace: this._id,
+			...data
+		});
+	}
+
+	/*async*/ getName() {
+		return this._send('workspace_name');
+	}
+
+	/*async*/ setName(name) {
+		return this._send('workspace_set_name', { name });
+	}
+
+	/*async*/ getDescription() {
+		return this._send('workspace_description');
+	}
+
+	/*async*/ setDescription(description) {
+		return this._send('workspace_set_description', { description });
+	}
+
+	async createProject(project) {
+		await this._send('workspace_create_project', { project });
+		return new Project(this._id, project, this._prefix);
+	}
+
+	getProject(project) {
+		return new Project(this._id, project, this._prefix);
+	}
+
+	/*async*/ getProjects() {
+		return this._send('workspace_projects');
+	}
+
+	/*async*/ deleteProject(project) {
+		return this._send('workspace_delete_project', { project });
+	}
+
+	getTicket(ticket) {
+		return new Workspace(this._id, ticket, this._prefix);
+	}
+}
+
+export class MemoryWorkspace extends Workspace {
+	constructor(id) {
+		super(id, 'mem');
+	}
+
+	static async open(author, email) {
+		const id = await invoke('mem_workspace_open', { author, email });
+		return new MemoryWorkspace(id);
+	}
+}
+
+export class GitWorkspace extends Workspace {
+	constructor(id) {
+		super(id, 'git');
+	}
+
+	static async open(remote) {
+		const id = await invoke('git_workspace_open', { remote });
+		return new GitWorkspace(id);
+	}
+}
--- minimap-app/www-src/js/index.mjs
+++ minimap-app/www-src/js/index.mjs
@@ -8,7 +8,7 @@ import systemTheme from 'minimap/js/util/s-theme.mjs';
 import Root from 'minimap/js/module/Root.mjs';
 import ErrorView from 'minimap/js/module/ErrorView.mjs';
 
-import { invoke } from '@tauri-apps/api/tauri';
+import { MemoryWorkspace } from 'minimap/js/api.mjs';
 
 import './reset.css';
 import './global.css';
@@ -59,24 +59,25 @@ S.root(() => {
 	document.body.prepend(<Root view={currentView} fadeTime={150} />);
 
 	// TODO DEBUG: test tauri API
-	const remote = 'git@github.com:Qix-/test-minimap.git';
-	let workspace;
-	invoke('workspace_open', { remote })
-		.then(ws => {
-			workspace = ws;
-			console.log('workspace_open():', workspace);
-			return invoke('workspace_name', {workspace});
-		})
-		.then(name => {
-			console.log('workspace_name():', name);
-			return invoke('workspace_set_name', {workspace, name: 'test-minimap'});
-		})
-		.then(e => {
-			console.log('workspace_set_name():', e);
-			return invoke('workspace_name', {workspace});
-		})
-		.then(name => {
-			console.log('workspace_name():', name);
-		})
-		.catch(err => console.error(err));
+	window.doItNow = async () => {
+		console.log('opening memory workspace...');
+		const workspace = await MemoryWorkspace.open(
+			'Max Mustermann',
+			'max@example.com'
+		);
+		console.log('opened workspace with name:', await workspace.getName());
+		console.log('setting workspace name...');
+		await workspace.setName('Test Workspace');
+		console.log('workspace name is now:', await workspace.getName());
+		console.log("creating project 'TEST'...");
+		const project = await workspace.createProject('TEST');
+		console.log('creating a ticket');
+		const ticket = await project.createTicket();
+		console.log('ticket slug:', ticket.slug);
+		console.log('setting ticket title and first comment...');
+		await ticket.setTitle('Test Ticket');
+		await ticket.addComment('This is a test ticket.');
+		console.log('ticket title:', await ticket.getTitle());
+		console.log('ticket comments:', await ticket.getComments());
+	};
 });
